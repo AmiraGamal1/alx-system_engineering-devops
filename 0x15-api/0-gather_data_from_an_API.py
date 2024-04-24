c9759eecd57ce1bc3b4a2369f0670e6933a6241f
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-04-24 23:34:58 +0200

    fix error

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,22 @@
 
 
 from requests import get
-import sys
+from sys import argv
 
 
 if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com'
-    todo_url = url + "/user/{}/todos".format(sys.argv[1])
-    name_url = url + "/users/{}".format(sys.argv[1])
-    todo_list = get(todo_url).json()
+    main_url = 'https://jsonplaceholder.typicode.com'
+    todo_url = main_url + "/user/{}/todos".format(argv[1])
+    name_url = main_url + "/users/{}".format(argv[1])
+    todo_result = get(todo_url).json()
     name_result = get(name_url).json()
 
-    todo_len = len(todo_list)
-    todo_complete = len([todo for todo in todo_list
+    todo_num = len(todo_result)
+    todo_complete = len([todo for todo in todo_result
                          if todo.get("completed")])
-    employee = name_result.get("name")
+    name = name_result.get("name")
     print("Employee {} is done with tasks({}/{}):"
-          .format(employee, todo_complete, todo_len))
-    for todo in todo_list:
+          .format(name, todo_complete, todo_num))
+    for todo in todo_result:
         if (todo.get("completed")):
             print("\t {}".format(todo.get("title")))

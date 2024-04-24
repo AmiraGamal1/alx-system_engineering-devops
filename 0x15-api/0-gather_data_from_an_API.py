978766871659854146b03587d7e00e475c906f59
Amira Nasr Aldein <amiragamal098@gmail.com>
2024-04-24 23:09:24 +0200

    json placeholder

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""use json plasceholder to get employee todo list"""
+
+
+from requests import get
+import sys
+
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com'
+    todo_url = url + "/user/{}/todos".format(sys.argv[1])
+    name_url = url + "/usrs/{}".format(sys.argv[1])
+    todo_list = get(todo_url).json()
+    name = get(name_url).json()
+
+    todo_len = len(todo_list)
+    todo_complete = len([todo for todo in todo_list
+                         if todo.get("completed")])
+    employee = name.get("name")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee, todo_complete, todo_len))
+    for todo in todo_list:
+        if (todo.get("completed")):
+            print("\t {}".format(todo.get("title")))
